cc14743817ad4f0afbd974b6fc0843d62ba2b017
RubenVercaemst <ruben.vercaemst@gmail.com>
2021-09-03 18:55:44 +0200

    Color navbar scroll

--- scripts.js
+++ scripts.js
@@ -3,8 +3,10 @@ window.onscroll = function() {
   var currentScrollPos = window.pageYOffset;
   if (prevScrollpos > currentScrollPos) {
     document.getElementById("navbar").style.top = "0";
+    document.getElementById("navbar").style.backgroundColor = "#1a2530";
   } else {
     document.getElementById("navbar").style.top = "-" + document.getElementById("navbar").offsetHeight + "px";
+    // document.getElementById("navbar").style.backgroundColor = "transparent";
   }
   prevScrollpos = currentScrollPos;
 }
